44a7f34cf56c57c42cc9eaea0db035487ba209ab
zver <zver@zver.ru>
2023-05-29 21:08:05 +0300

    2 step

--- str_func.py
+++ str_func.py
@@ -1,14 +1,8 @@
-def func3(s: str):
-    """ func3 docstring3 """
-    return s.lower()
-
 def func1(s: str):
     """ func1 doc1 """
-    """ func2 doc2 """
     return s.upper()
 
 
-
-def func2(s: str):
-    """ func2 docstring """
-    return s.capitalize()
+def funcdf(s: str):
+    """ func1 doc1 """
+    return s.strip()
