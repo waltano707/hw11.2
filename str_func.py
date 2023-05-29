cd139d50afae7cd0d9863e9e857ee28e81228222
waltano707 <98036573+waltano707@users.noreply.github.com>
2023-05-29 21:06:58 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,14 +1,3 @@
-def func3(s: str):
-    """ func3 docstring3 """
-    return s.lower()
-
 def func1(s: str):
-    """ func1 doc1 """
-    """ func2 doc2 """
-    return s.upper()
-
-
-
-def func2(s: str):
-    """ func2 docstring """
-    return s.capitalize()
+    """ func1 docstring1 """
+    return s.lower()
